117d92dfc8ea052e8e1f8fbf146f47d1b1a03c41
svenbaertschi <will_2235@hotmail.com>
2017-07-28 03:39:54 -0700

    ocd stuff, wasting time

--- homepage/views.py
+++ homepage/views.py
@@ -4,7 +4,7 @@ def index(request):
     return render(request, 'homepage/home.html')
 
 def contact(request):
-    return render(request, 'temptext.html', {'message':  "<a href='mailto:will@ubctacs.org' style='text-decoration:none;'><h1>will@ubctacs.org</h1></a>"})
-  
+    return render(request, 'temptext.html', {'message':  "<a href='mailto:will@ubctacs.org' class='link-dark'>will@ubctacs.org</a>"})
+
 def developers(request):
-    return render(request, 'temptext.html', {'message':  "<a href='https://github.com/svenbaertschi/mako' style='text-decoration:none;'><h1>github (help here)</h1></a>"})
+    return render(request, 'temptext.html', {'message':  "<a href='https://github.com/svenbaertschi/mako' class='link-dark'>github (help here)</a>"})
