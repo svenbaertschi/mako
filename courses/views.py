5389c6b20cfc666128149d11a50c0a0b8965f4cd
svenbaertschi <will_2235@hotmail.com>
2017-07-28 02:52:27 -0700

    use cdn instead of local (no reason, just felt like it)

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-        return render(request, 'temptext.html', {'message':'<h1>nothing here yet</h1>'})
+    return render(request, 'temptext.html', {'message':'<h1>nothing here yet</h1>'})
--- newsite/urls.py
+++ newsite/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^', include('homepage.urls')),
+    url(r'^', include('homepage.urls')),
 	url(r'^admin/', admin.site.urls),
-	url(r'^apps/', include('courses.urls')),
+    url(r'^apps/', include('courses.urls')),
     url(r'^weather/', include('weather.urls')),
 ]
